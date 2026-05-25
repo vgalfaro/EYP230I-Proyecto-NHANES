1de420b0834ba2ee2059e465101d97a7ad87e793
IsidoraGuinez <isiguinez@uc.cl>
2026-05-25 17:05:21 -0400

    add graficos

--- eda/eda_vol2.R
+++ eda/eda_vol2.R
@@ -1,7 +1,7 @@
 
 library(foreign)
 library(dplyr)
-
+library(ggplot2)
 
 # 1. CARGA Y UNIÓN DE DATOS 
 
@@ -63,19 +63,49 @@ datos_limpios <- datos_limpios %>%
 print("Datos finales listos para el EDA:")
 summary(datos_limpios)
 
+# 4. EDA
+
+#  Distribución Univariada de colesterol y circunferencia de cintura
+p1 <- ggplot(datos_limpios, aes(x = Colesterol)) + 
+  geom_histogram(bins = 30, fill = "steelblue", color = "white") +
+  theme_minimal() + labs(title = "Distribución del Colesterol Total ($mg/dL$)")
+ggsave("eda_dist_colesterol.png", plot = p1)
+
+p2 <- ggplot(datos_limpios, aes(x = Cintura)) + 
+  geom_histogram(bins = 30, fill = "darkorange", color = "white") +
+  theme_minimal() + labs(title = "Distribución de la Circunferencia de Cintura ($cm$)")
+ggsave("eda_dist_cintura.png", plot = p2)
 
 
 
+#
+p3 <- ggplot(datos_limpios, aes(x = Cintura, y = Colesterol)) +
+  geom_point(alpha = 0.3, color = "gray30") +
+  geom_smooth(method = "loess", color = "red", se = TRUE) +
+  theme_minimal() +
+  labs(title = "Relación Cruda: Cintura vs Colesterol Total",
+       x = "Circunferencia de Cintura ($cm$)", y = "Colesterol Total ($mg/dL$)")
+ggsave("eda_bivariado_crudo.png", plot = p3)
+
+#Relación Cintura-Colesterol separada por Sexo
+p4 <- ggplot(datos_limpios, aes(x = Cintura, y = Colesterol, color = Sexo)) +
+  geom_point(alpha = 0.2) +
+  geom_smooth(method = "lm", se = FALSE) +
+  theme_minimal() +
+  labs(title = "Relación Cintura vs Colesterol Estratificada por Sexo",
+       x = "Circunferencia de Cintura ($cm$)", y = "Colesterol Total ($mg/dL$)")
+ggsave("eda_estratificado_sexo.png", plot = p4)
 
 
-
-
-
-
-
-
-
-
+# Relación Cintura-Colesterol separada por Nivel Socioeconómico (NSE)
+p5 <- ggplot(datos_limpios, aes(x = Cintura, y = Colesterol)) +
+  geom_point(alpha = 0.2, aes(color = NSE_Grupo)) +
+  geom_smooth(method = "lm", color = "black") +
+  facet_wrap(~NSE_Grupo) +
+  theme_bw() +
+  labs(title = "Relación Cintura vs Colesterol por Nivel Socioeconómico",
+       x = "Circunferencia de Cintura ($cm$)", y = "Colesterol Total ($mg/dL$)")
+ggsave("eda_estratificado_nse.png", plot = p5)
 
 
 
